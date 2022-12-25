07f10e226c277bece381e9db90ed23852f123f4c
AfzalHaq <113984344+AfzalHaq61@users.noreply.github.com>
2022-12-26 00:58:47 +0500

    Tailwind talk with peter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,25 @@
 </head>
 
 <body>
+    <!-- here we will use extend css stylings -->
     <div class="flex justify-center p-[100px]">
-        <div class="bg-yellow-100 p-[100px] border-2 border-black mr-[10px]">
+        <div class="bg-regal-blue p-[100px] border-2 border-black mr-[10px]">
             <h1 class="font-bold">Monday</h1>
         </div>
         <div class="bg-green-100 p-[100px] border-2 border-black">
             <h1 class="font-bold">Tuesday</h1>
+        </div>
+    </div>
+    <!-- same spaces in div b/c of space-y-10 -->
+    <div class="space-y-10 p-[10px] bg-slate-400">
+        <div>
+            htmlspecialchars_decode
+        </div>
+        <div>
+            headers_list
+        </div>
+        <div>
+            kdsl
         </div>
     </div>
     <hr>
